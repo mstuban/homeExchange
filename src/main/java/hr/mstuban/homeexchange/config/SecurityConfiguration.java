1f319006c27b687cf8af4bf2e8e8ae83480ca29b
mstuban <stuban.marko@gmail.com>
2017-05-09 21:09:07 +0200

    Set up spring boot, liquibase and basic Security Configuration

---
+++ src/main/java/hr/mstuban/homeexchange/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package hr.mstuban.homeexchange.config;
+
+/**
+ * Created by mstuban on 09.05.17..
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+}
+
