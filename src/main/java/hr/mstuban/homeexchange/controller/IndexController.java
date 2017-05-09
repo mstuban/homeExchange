5cb6902a91bcd0f73077e4cc9ec36d45d19cc3aa
mstuban <stuban.marko@gmail.com>
2017-05-09 21:57:31 +0200

    Dodan bootstrap, custom css and js i IndexController

---
+++ src/main/java/hr/mstuban/homeexchange/controller/IndexController.java
@@ -0,0 +1,15 @@
+package hr.mstuban.homeexchange.controller;
+
+/**
+ * Created by mstuban on 09.05.17..
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    String getHome(){
+        return "index";
+    }
+}
---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,3 @@
+/**
+ * Created by marko on 09.05.17..
+ */
